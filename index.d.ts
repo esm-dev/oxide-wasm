64d18a257daa1707e12d1e0f28e864eba079e7c9
Je Xia <i@jex.me>
2026-02-21 13:00:25 +0800

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
+type SyncInitInput = BufferSource | WebAssembly.Module;
+type InitInput = RequestInfo | URL | Response | SyncInitInput;
+
+export function initSync(module: SyncInitInput): void;
+export function init(module_or_path: InitInput | Promise<InitInput>): Promise<void>;
 export function extract(input: string): string[];
+export default init;
