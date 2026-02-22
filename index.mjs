36bd2b4d120cea3e84cec093ac18a88e58e6b00a
Je Xia <i@jex.me>
2026-02-22 14:03:10 +0800

    Fix loading wasm form esm.sh CDN

--- index.mjs
+++ index.mjs
@@ -15,20 +15,20 @@ export async function init(module_or_path) {
   const importUrl = import.meta.url;
   if (!module_or_path && importUrl.startsWith("file://")) {
     const { pathname: filename } = new URL("./pkg/oxide_wasm_bg.wasm", importUrl);
-    let wasmBytes;
+    let wasmData;
     if (globalThis.Deno) {
-      wasmBytes = await Deno.readFile(filename);
+      wasmData = await Deno.readFile(filename);
     } else if (globalThis.Bun) {
-      wasmBytes = await Bun.file(filename).arrayBuffer();
+      wasmData = await Bun.file(filename).arrayBuffer();
     } else {
       const moduleSpecifier = `node:fs/promises`;
       const fsPromise = await import(moduleSpecifier); // <- use variable to skip deno-lsp analyzing
-      wasmBytes = await fsPromise.readFile(filename);
+      wasmData = await fsPromise.readFile(filename);
     }
-    initWasmSync({ module: wasmBytes });
+    initWasmSync({ module: wasmData });
     return;
   }
-  const esmshBaseUrl = "https://esm.sh/@esm.sh/oxide-wasm@";
+  const esmshBaseUrl = "https://esm.sh/oxide-wasm@";
   if (!module_or_path && importUrl.startsWith(esmshBaseUrl)) {
     const version = importUrl.slice(esmshBaseUrl.length).split("/", 1)[0];
     module_or_path = esmshBaseUrl + version + "/pkg/oxide_wasm_bg.wasm";
