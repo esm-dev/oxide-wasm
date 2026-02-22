631542eea69254a915d7040e39b88d62eb8bc52c
Je Xia <i@jex.me>
2026-02-22 14:12:30 +0800

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,10 @@ pub fn extract(input: &str) -> Vec<JsValue> {
     Extractor::new(input.as_bytes())
         .extract()
         .into_iter()
-        .map(|v| match v {
-            Extracted::Candidate(bytes) => JsValue::from_str(unsafe { from_utf8_unchecked(bytes) }),
-            Extracted::CssVariable(bytes) => {
-                JsValue::from_str(unsafe { from_utf8_unchecked(bytes) })
-            }
+        .map(|e| match e {
+            Extracted::Candidate(bytes) => bytes,
+            Extracted::CssVariable(bytes) => bytes,
         })
+        .map(|b| JsValue::from_str(unsafe { from_utf8_unchecked(b) }))
         .collect::<Vec<_>>()
 }
